31f107c09dd2fa31dc07874e614a05125599b572
Felima23 <felipedecastroelima669@gmail.com>
2025-05-06 17:23:01 -0300

    configuracao de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/<fclima.github.io>' element={<Home/>}/>
-                <Route path='/sobre<fclima.github.io>' element={<About/>}/>
-                <Route path='/projetos<fclima.github.io>' element={<Projects/>}/>
+                <Route path="/fclima.github.io/" element={<Home/>}/>
+                <Route path="/fclima.github.io/sobre" element={<About/>}/>
+                <Route path="/fclima.github.io/projetos" element={<Projects/>}/>
             </Routes>        
         </BrowserRouter>
     )
